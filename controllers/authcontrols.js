06c32a9d1aaa881843e1c1f0e5a11b3c5b91e838
KARTHICK <krtik3366@gmail.com>
2023-06-10 18:17:12 +0530

    email text altered

--- controllers/authcontrols.js
+++ controllers/authcontrols.js
@@ -23,27 +23,15 @@ const forgotPassword = async (req, res, next) => {
 
         const resetLink = `http://localhost:3300/reset-password?token=${token}`;
 
-        //     const emailText = `
-        //   Hello ${user.username},
-
-        //   You have requested to reset your password. Please click the link below to proceed:
-        //   ${resetLink}
-
-        //   If you did not request this, please ignore this email.
-
-        //   Best regards,
-        //   ${user.company_name}
-        // `;
-
         const emailText = `
         <h1>Password Reset Request</h1>
-  <p>Hello ${user.username} ,</p>
-  <p>We received a request to reset your password for your account. To proceed with the password reset, please click on the following link:</p>
-  <a href="${resetLink}">Reset Password</a>
-  <p>If you did not initiate this request, please ignore this email. Your password will remain unchanged.</p>
-  <p>Thank you,</p>
-  <p>${user.company_name}</p>
-  `;
+        <p>Hello ${user.username} ,</p>
+        <p>We received a request to reset your password for your account. To proceed with the password reset, please click on the following link:</p>
+        <a href="${resetLink}">Reset Password</a>
+        <p>If you did not initiate this request, please ignore this email. Your password will remain unchanged.</p>
+        <p>Thank you,</p>
+        <p>${user.company_name}</p>
+        `;
 
         await sendEmail(user.email, 'Password Reset Request', emailText);
 
